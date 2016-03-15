5ddb9c182fb3fd96d2cc0b35866259a1480f7791
etcool <wmkdes@gmail.com>
2016-03-15 09:50:39 -0500

    Proyecto Store Online
    Upload Project Store Online

---
+++ storeOnlineBoot/src/main/java/com/innovandotek/StoreOnlineBootApplication.java
@@ -0,0 +1,12 @@
+package com.innovandotek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreOnlineBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreOnlineBootApplication.class, args);
+	}
+}
